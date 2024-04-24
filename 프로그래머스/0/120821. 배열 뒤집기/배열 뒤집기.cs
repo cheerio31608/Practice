63d8a35ade4ffe6ce9286751b5118330f91bb00c
KIMSHINWOO <swkim9985@naver.com>
2024-04-24 09:18:41 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.33 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = num_list;
+        Array.Reverse(answer);
+        return answer;
+    }
+}
